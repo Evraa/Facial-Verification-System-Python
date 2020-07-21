f3dfec223f9329bac25b7a5adcc69809f4afab9d
Evraa <evramyousef@gmail.com>
2020-07-21 05:45:00 +0200

    added prototype for face/shape detector

---
+++ Demo/src/facial_landmarks.py
@@ -0,0 +1,48 @@
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+from face_utils import *
+from auxilary import *
+
+def calc_distances(shape):
+    pass
+        
+
+def detect_face():
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument("-p", "--shape-predictor", required=True,\
+        help="path to facial landmark predictor")
+
+    ap.add_argument("-i", "--image", required=True,\
+        help="path to input image")
+
+    args = vars(ap.parse_args())
+
+    # initialize dlib's face detector (HOG-based) and then create
+    # the facial landmark predictor
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(args["shape_predictor"])
+
+    # load the input image, resize it, and convert it to grayscale
+    image = cv2.imread(args["image"])
+    image = imutils.resize(image, width=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # detect faces in the grayscale image
+    rects = detector(gray, 1)
+
+
+    # loop over the face detections
+    for (i, rect) in enumerate(rects):
+        shape = predictor(gray, rect)
+        shape = shape_to_np(shape)
+        #I am assuming only one face is represented in this image
+        calc_distances(shape)
+        
+    # show the output image with the face detections + facial landmarks
+    # cv2.imshow("Output", image)
+    # cv2.waitKey(0)
