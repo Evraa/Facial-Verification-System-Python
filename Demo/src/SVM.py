83aac7beac3faf401c4822231ce8433295fc21f8
Maggie C <maggie01pd2018@sasstudent.org>
2020-07-29 21:40:34 -0700

    svm

--- Demo/src/SVM.py
+++ Demo/src/SVM.py
@@ -1,7 +1,11 @@
 import auxilary
 import numpy as np
+from sklearn import svm
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
 
-def svm():
+
+def svm_compare():
     '''
     TODO: implement svm
     '''
@@ -23,3 +27,22 @@ def svm():
     labels = np.ones([N,1])
     labels[data_labels] = 0
 
+    clf = svm.SVC(gamma=0.001, C=100)
+    X, y = inputs, np.ravel(labels)
+    # shuffles the date to save 20% of data for testing
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        shuffle=True,
+        random_state=42,
+    )
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    print("predictions: \n" , y_pred)
+    print("\naccuracy: \n",np.array(y_pred == y_test)[:25])
+    print('\npercentage correct: ', 100 * np.sum(y_pred == y_test) / len(y_test))
+
+
+svm_compare()
