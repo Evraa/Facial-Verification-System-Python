c87a283f2da7d746b2c13fca5971619943a9cbcb
Evraa <evramyousef@gmail.com>
2020-08-09 13:18:17 +0200

    added unique class for each person we're detecting

--- Demo/src/NN.py
+++ Demo/src/NN.py
@@ -1,6 +1,5 @@
 import auxilary
-# import face_recognition
-# import show_tests
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import os
 
@@ -13,15 +12,26 @@ def prepare_data():
     data_inputs = (data.iloc[:,:D])
     inputs = np.zeros([N,D])
     inputs = np.array(data_inputs)
+
     labels = np.zeros([N,1])
     labels = np.array(data.iloc[:,D])
+
+    names_encode = LabelEncoder().fit(labels)
+    Y = names_encode.transform(labels) # in range of [0:252]
+    unique_count = len(set(labels))
+    #Transofrm the Y into shape of [5050, 253] with all equals zeros, except for the correct label
+    y = np.zeros([N,unique_count])
+    for i, row in enumerate(y):
+        row[Y[i]] = 1
+
     print (f'Inputs shape: {inputs.shape}')
-    print (f'Outputs shape: {labels.shape}')
-    return inputs, labels
+    print (f'Outputs shape: {y.shape}')
+    return inputs, y
 
 def train():
     X, y = prepare_data()
 
     
+
 if __name__ == "__main__":
     train()
